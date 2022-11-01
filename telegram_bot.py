4caa33b1fbd1b536f0d83a60fc43b4b73aee3d00
Dmitry Gaponov <gaponov@isands.ru>
2022-11-01 18:51:04 +0700

    send messages from telegram to server

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,9 +1,13 @@
 import logging
 import os
 
+#https://api.vk.com/method/messages.send?access_token=vk1.a.0TVflx5sRYme5-buZhMYYoRezDy96ESDxKOBVlTPikEJcN4fyHhst-QZj0IGa_5fBtObTMsnaQTfXKxTqhNIpq2vOc3TAEdV6xuOH16FJYdiBHgkHuH6aBDAJSbTDjPeUGTpFfOhsxR16eiWRqBGQrSkfRvjkRBju9xUkrBd4Gwo11NxNubBwwFOaXcL4aPz7VEdPGi_8nOiQbkJUM6cMw&v=5.131&random_id=8&message=Все знают кто тут главный бот...&peer_id=2000000001
+#https://api.telegram.org/bot5691524154:AAGYDE1AxXxeOpffFKmvf_X6zMj4f09JhE0/sendmessage?chat_id=-1001753104086&text=Можно и Игорька на кукан посадить
+
 from telegram import Update, ForceReply, ReplyMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
+import rcon_connect
 from source_query import SourceQuery
 from utils import get_top_players_message
 
@@ -67,6 +71,17 @@ def echo(update: Update, context: CallbackContext) -> None:
             update.message.reply_text(s, quote=False)
     elif update.message.text.lower() == 'топ':
         update.message.reply_text(get_top_players_message(), quote=False)
+    elif len(update.message.text.strip()) > 5 and update.message.text.lower()[:5] == 'всем:':
+        message_to_server = update.message.text[5:].strip()
+        try:
+            command = 'send_message_rcon "ТГ" "' + update.message.from_user.full_name + '" "' + message_to_server + '"'
+            response = rcon_connect.send_command(command)
+            if response:
+                update.message.reply_text('Сообщение отправлено', quote=True)
+        except Exception as e:
+            update.message.reply_text('Ошибка при отправке сообщения', quote=True)
+            print(e)
+
 
 
 def main() -> None:
@@ -81,7 +96,7 @@ def main() -> None:
     #dispatcher.add_handler(CommandHandler("start", start))
     #dispatcher.add_handler(CommandHandler("help", help_command))
 
-    # on non command i.e message - echo the message on Telegram
+    # on non command i.e. message - echo the message on Telegram
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo, edited_updates=False))
 
     # Start the Bot
