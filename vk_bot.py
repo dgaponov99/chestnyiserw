cd36726e9f739faf938fad77f5af41bca0bddf5c
Dmitry Gaponov <gaponov@isands.ru>
2022-11-05 04:45:51 +0700

    try

--- vk_bot.py
+++ vk_bot.py
@@ -45,64 +45,67 @@ def main():
 
     # todo: need callback
     while True:
-        for event in longpoll.listen():
-            try:
-                if event.type == VkBotEventType.MESSAGE_NEW:
-                    print(event)
+        try:
+            for event in longpoll.listen():
+                try:
+                    if event.type == VkBotEventType.MESSAGE_NEW:
+                        print(event)
 
-                    if event.object.message['peer_id'] != 2000000001:
-                        continue
+                        if event.object.message['peer_id'] != 2000000001:
+                            continue
 
-                    print(event.object.message['text'])
-                    if event.object.message['text'].lower() == 'сервер':
-                        try:
-                            query = SourceQuery('193.19.118.81', 27025)
+                        print(event.object.message['text'])
+                        if event.object.message['text'].lower() == 'сервер':
+                            try:
+                                query = SourceQuery('193.19.118.81', 27025)
 
-                            s = 'Информация о сервере:\n\n'
+                                s = 'Информация о сервере:\n\n'
 
-                            res = query.get_info()
-                            s += 'Название: ' + res['Hostname'] + '\n'
-                            s += 'Адрес сервера: 193.19.118.81:27025\n'
-                            s += 'Карта: ' + res['Map'] + '\n'
-                            s += 'Онлайн: ' + "%i/%i" % (res['Players'], res['MaxPlayers']) + '\n'
+                                res = query.get_info()
+                                s += 'Название: ' + res['Hostname'] + '\n'
+                                s += 'Адрес сервера: 193.19.118.81:27025\n'
+                                s += 'Карта: ' + res['Map'] + '\n'
+                                s += 'Онлайн: ' + "%i/%i" % (res['Players'], res['MaxPlayers']) + '\n'
 
-                            s += '\n'
+                                s += '\n'
 
-                            players = query.get_players()
-                            s += 'Игроки онлайн:\n'
-                            if len(players) > 0:
-                                for player in players:
-                                    s += "{id}. {Name}, фраги: {Frags}, время: {PrettyTime}".format(**player) + '\n'
-                            else:
-                                s += 'Сервер пуст &#128549;'
-                            # print(s)
+                                players = query.get_players()
+                                s += 'Игроки онлайн:\n'
+                                if len(players) > 0:
+                                    for player in players:
+                                        s += "{id}. {Name}, фраги: {Frags}, время: {PrettyTime}".format(**player) + '\n'
+                                else:
+                                    s += 'Сервер пуст &#128549;'
+                                # print(s)
 
-                            query.disconnect()
-                            write_msg(s)
-                        except Exception as e:
-                            write_msg('Не могу соединиться с сервером &#128549;')
-                    elif event.object.message['text'].lower() == 'топ':
-                        write_msg(get_top_players_message())
-                    elif len(event.object.message['text'].strip()) > 5 and \
-                            event.object.message['text'][:5].lower() == 'всем:':
-                        print('я тут')
-                        message_to_server = event.object.message['text'][5:].strip()
-                        name, lastname = get_name(event.object.message['from_id'])
-                        print(name)
-                        print('Отправляю сообщение...')
-                        try:
-                            command = 'send_message_rcon "ВК" "' + name + ' ' + lastname + '" "' \
-                                      + message_to_server + '"'
-                            response = rcon_connect.send_command(command)
-                            telegram_bot_sendtext('\\[ВК] ' + name + ' ' + lastname + ': ' + message_to_server)
-                            print(response)
-                            if response:
-                                write_msg(name + ', твое сообщение отправлено')
-                        except Exception as e:
-                            write_msg(name + ', ошибка, сообщение не отправлено')
-                            print(e)
-            except Exception as e:
-                print(e)
+                                query.disconnect()
+                                write_msg(s)
+                            except Exception as e:
+                                write_msg('Не могу соединиться с сервером &#128549;')
+                        elif event.object.message['text'].lower() == 'топ':
+                            write_msg(get_top_players_message())
+                        elif len(event.object.message['text'].strip()) > 5 and \
+                                event.object.message['text'][:5].lower() == 'всем:':
+                            print('я тут')
+                            message_to_server = event.object.message['text'][5:].strip()
+                            name, lastname = get_name(event.object.message['from_id'])
+                            print(name)
+                            print('Отправляю сообщение...')
+                            try:
+                                command = 'send_message_rcon "ВК" "' + name + ' ' + lastname + '" "' \
+                                          + message_to_server + '"'
+                                response = rcon_connect.send_command(command)
+                                telegram_bot_sendtext('\\[ВК] ' + name + ' ' + lastname + ': ' + message_to_server)
+                                print(response)
+                                if response:
+                                    write_msg(name + ', твое сообщение отправлено')
+                            except Exception as e:
+                                write_msg(name + ', ошибка, сообщение не отправлено')
+                                print(e)
+                except Exception as e:
+                    print(e)
+        except Exception as e:
+            print(e)
 
 
 if __name__ == "__main__":
