88e7c3eb26d53a096fdcd5cb4a8427a97aee7a60
Dmitry Gaponov <gaponov@isands.ru>
2023-01-12 22:02:52 +0700

    add start py with main

--- start.py
+++ start.py
@@ -1,6 +1,8 @@
 from source_query.SourceQuery import SourceQuery
+from tg.telegram_bot import main
 
 if __name__ == '__main__':
-    query = SourceQuery('37.230.137.233', 33333)
-    #query = SourceQuery('193.19.118.81', 27025)
-    print(query.get_server())
+    # query = SourceQuery('37.230.137.233', 33333)
+    # #query = SourceQuery('193.19.118.81', 27025)
+    # print(query.get_server())
+    main()
